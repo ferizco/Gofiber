1364c807abe5ca520c9e8c20aa72612665f87a7f
Ferizco <ferizco@ptmitrabhakti.com>
2024-02-14 10:55:58 +0700

    edit: clean code

--- static/script.js
+++ static/script.js
@@ -1,7 +1,7 @@
+// function for message box when deleting
 function confirmDelete(id, judul) {
-    // Display confirmation dialog using SweetAlert
     Swal.fire({
-        title: `Are you sure delete ${judul}?`, // Use the book title from the data attribute
+        title: `Are you sure delete ${judul}?`, 
         text: 'You will not be able to recover this item!',
         icon: 'warning',
         showCancelButton: true,
@@ -10,35 +10,32 @@ function confirmDelete(id, judul) {
         confirmButtonText: 'Yes, delete it!'
     }).then((result) => {
         if (result.isConfirmed) {
-            // If user confirms deletion, submit the form
-            document.getElementById('deleteForm' + id).submit(); // Use unique form ID
+            document.getElementById('deleteForm' + id).submit(); 
         }
     });
-    // Prevent default form submission
     return false;
 }
 
+// function for rendering star in table 
 function renderStars(rating) {
     let stars = '';
     for (let i = 0; i < 5; i++) {
         if (i < rating) {
-            stars += '<i class="fas fa-star gold-star"></i>'; // Ikuti kelas Font Awesome untuk ikon bintang yang diisi
+            stars += '<i class="fas fa-star gold-star"></i>'; 
         } else {
-            stars += '<i class="far fa-star gold-star"></i>'; // Ikuti kelas Font Awesome untuk ikon bintang kosong
+            stars += '<i class="far fa-star gold-star"></i>'; 
         }
     }
     return stars;
 }
 
-// Ambil semua elemen dengan kelas "stars" dan ubah isi HTML-nya menjadi ikon bintang
 document.querySelectorAll('.stars').forEach(function(starElement) {
-    const rating = parseInt(starElement.textContent); // Ambil nilai rating dari konten elemen
-    starElement.innerHTML = renderStars(rating); // Ubah isi HTML elemen menjadi ikon bintang
+    const rating = parseInt(starElement.textContent); 
+    starElement.innerHTML = renderStars(rating); 
 });
 
 $(document).ready(function() {
     $('#dataTable').DataTable({
         "searching": true,
-        // Atur opsi lainnya sesuai kebutuhan Anda
     });
 });
